530773eaa11b00af38eb32f168d8e19ee8013cc0
gamado14 <60581270+gamado14@users.noreply.github.com>
2020-05-12 16:54:18 -0400

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-require_once 'includes/auth.php';
 ?>
 
 <!DOCTYPE html>
@@ -17,22 +16,19 @@ require_once 'includes/auth.php';
 
 <?php
 
-include_once 'includes/header.php';
-require_once 'includes/login.php';
-require_once 'includes/functions.php';
+include_once 'header.php';
+require_once 'login.php';
+require_once 'functions.php';
 
-if (isset($_POST['submit'])) { 
+if (isset($_POST['submit'])) { //check if the form has been submitted
 	if ( empty($_POST['username']) || empty($_POST['password']) ) {
 		echo "<p>Please fill out all of the form fields!</p>";
 	} else {
-		$conn = new mysqli($hn, $un, $pw, $db);
+		$conn = new mysqli($hostname, $username, $password, $database);
 		if ($conn->connect_error) die($conn->connect_error);
 		$username = sanitizeMySQL($conn, $_POST['username']);
 		$password = sanitizeMySQL($conn, $_POST['password']);			
-		$salt1 = "*kw&!h*";  
-		$salt2 = "@pxg!@";  
-		$password = hash('ripemd128', $salt1.$password.$salt2);
-		$query  = "SELECT first_name, last_name FROM users WHERE username='$username' AND password='$password'"; 
+		$query  = "SELECT first_name, last_name FROM user WHERE username='$username' AND password='$password'"; 
 		$result = $conn->query($query);    
 		if (!$result) die($conn->error); 
 		$rows = $result->num_rows;
@@ -40,16 +36,23 @@ if (isset($_POST['submit'])) {
 			$row = $result->fetch_assoc();
 			$_SESSION['first_name'] = $row['first_name'];
 			$_SESSION['last_name'] = $row['last_name'];	
-			header("Location: viewcolle.php"); 			
+			$goto = empty($_SESSION['goto']) ? 'index.php' : $_SESSION['goto'];			
+			header('Location: ' . $goto);
+			exit;		
 		} else {
-			echo "<p>Oh no! The password or username is not valid. Try again.</p>";
+			echo "<p>Invalid username/password combination!</p>";
 		}
 	}
 }
 ?>
 
+<!--Sign_in Section-->
+<div class="sign_in">
+<div class="sign_in-text"><h1>Learn more about art</h1><h2>Explore your collections and favorite artists!</h2></div>
+</div>
+
 <fieldset style="width:30%"><legend>Sign in</legend>
-<form method="POST" action="">
+<form method="POST" action="sign_in.php">
 Username:<br><input type="text" name="username" size="40"><br>
 Password:<br><input type="password" name="password" size="40"><br>
 <input type="submit" name="submit" value="Submit">
