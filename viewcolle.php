eee190f0e5afaac9604ea64a4fecbd90eae722b4
gamado14 <60581270+gamado14@users.noreply.github.com>
2020-05-12 16:58:07 -0400

    Update viewcolle.php

--- viewcolle.php
+++ viewcolle.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-require_once 'includes/auth.php';
+$_SESSION['username'] = 'username';
+$_SESSION['password'] = '';
+$_SESSION['time'] = time();	
+$_SESSION["user_id"] = 'user_id';
+
 ?>
 
 <!DOCTYPE html>
@@ -16,36 +20,80 @@ require_once 'includes/auth.php';
 <body>
 
 <?php
-include_once 'includes/header.php';
-require_once 'includes/login.php';
-require_once 'includes/functions.php';
+include_once 'header.php';
+require_once 'login.php';
+require_once 'functions.php';
+include_once 'auth.php';
 
 
-$conn = new mysqli($hn, $un, $pw, $db);
+$conn = new mysqli($hostname, $username, $password, $database);
 if ($conn->connect_error) die($conn->connect_error);
 
-if (isset($_GET['id'])) {
-	$id = sanitizeMySQL($conn, $_GET['id']);
-	$query = "SELECT * FROM collection, collection_item WHERE `user_id`= $_SESSION["user_id"]";
-	$result = $conn->query($query);
-	if (!$result) die ("The id is invalid. Please try again.");
-	$rows = $result->num_rows;
-	if ($rows == 0) {
-		echo "Sorry, there are no results for this user $id<br>";
+if (isset($_GET['submit'])) {
+	if (empty($_GET['name'])) {
+		echo "<p>Please fill out all of the form fields!</p>";
 	} else {
-		while ($row = $result->fetch_assoc()) {
-			echo '<h1>My Collection</h1>';
-			echo '<p>'.$row["collection_name"]'</p>'; 
-			echo $row["art_id"];			
+		$conn = new mysqli($hostname, $username, $password, $database);
+		if ($conn->connect_error) die($conn->connect_error);
+		$name = sanitizeMySQL($conn, $_GET['name']);
+		$query = "SELECT * FROM user NATURAL JOIN collection NATURAL JOIN collection_item NATURAL JOIN art NATURAL JOIN content NATURAL JOIN asset WHERE `user_id`=" .$_SESSION["user_id"];
+		$result = $conn->query($query);
+		if (!$result) {
+			die ("Database access failed: " . $conn->error);
+		} else {
+			$rows = $result->num_rows;
+			if ($rows) {
+				echo 
+				"<h1>Results</h1>
+				<table><tr>
+				
+				<th>art_id</th>
+				<th>title</th>
+				<th>artist_name</th>
+				<th>date_created</th>
+				<th>image_file</th>
+				<th>theme</th>
+				<th>content_file_name</th>
+				
+				</tr>";
+				
+				while ($row = $result->fetch_assoc()) {
+					echo "<tr>";
+					echo "<td>"
+					.$row["art_id"]."</td><td>"
+					.$row["title"]."</td><td>"
+					.$row["artist_name"]."</td><td>"
+					.$row["date_created"]."</td><td>"
+					.$row["theme"]."</td><td>"
+					.$row["content_file_name"]."</td><td>"
+					.$row["image_file"]."</td>";
+					echo "</tr>";
+				}
+				echo "</table>";
+			} else {
+				echo "<p>Sorry, there are no results for this query!</p>";
+			}
+			echo "<h2>Search Again</h2>";
 		}
 	}
-	echo "<p><a href=\"search.php\">Explore Art and Themes</a></p>";
-} else {
-	echo "No artwork was found with the id";
 }
-
-include_once 'footer.php';
 ?>
+<!--View_coll Section-->
+<div class="view_coll">
+<div class="view_coll-text"><h1>See your collection</h1><h2>Sign in</h2></div>
+</div>
+
+<!--Search Form-->
+<form method="get" action="viewcolle.php">
+	<fieldset>
+		<legend>My Collection</legend>
+		<input type="text" name="name" required><br> 
+		<input type="submit" name="submit">
+	</fieldset>
+</form>
+
+
+<?php include_once ('footer.php');?>
 
 </body>
 </html>
